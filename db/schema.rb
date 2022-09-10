3baf529c59b51d29b4c22874742870a41bb7764f
julian chadwick <noster91@gmail.com>
2022-09-10 18:02:28 -0300

    Added migration for adding rating on Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_10_181224) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_210107) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 
 end
